1fae8c9bc5a663ca067983295981649aa6b70654
johnLeavell <johnleavell@gmail.com>
2023-08-29 13:47:21 -0500

    completed assignment

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ActionController::Base
+
+  def index
+    render({ template: 'home/_home'})
+  end
+
+  def dice_roll
+    @num_dice = params.fetch("x").to_i
+      
+    @sides = params.fetch("y").to_i
+      
+    @rolls = []
+      
+      @num_dice.times do
+        die = rand(1..@sides)
+      
+        @rolls.push(die)
+      end
+      
+    render({ template: 'home/_dice_roll' })
+  end
+
+end
